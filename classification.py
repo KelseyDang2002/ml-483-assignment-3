468004b6dd012e0065965e440446ce7fe9065b17
KelseyDang2002 <kelseydang813@gmail.com>
2024-11-13 10:55:50 -0800

    file setup

---
+++ classification.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    print("classification")
+
+if __name__ == "__main__":
+    main()
---
+++ clustering.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    print("clustering")
+
+if __name__ == "__main__":
+    main()
